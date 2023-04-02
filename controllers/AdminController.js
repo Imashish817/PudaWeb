5aef685c540a916bb0d6128061c6dd11f3c586cf
Ashish jaiswal <imashishjaiswal99@gmail.com>
2023-04-02 13:38:40 +0530

    minor requrity flawas fix

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -4,10 +4,23 @@ class AdminController{
 
     static GetUnverifiedUsers= async (req,res)=>{
         console.log(req.query.aadharno)
+        let result=[];
         if(req.user.UserType === "Admin")
         {
             const a=await UserModel.find({"FilePath":{$exists:true}  ,UserType:"Normal" })
-            res.send({"data":a})
+            
+            a.forEach(user => {
+                let userObj={
+                    Name: user.Name,
+                    AadharNo: user.AadharNo,
+                    UserType: user.UserType,
+                    FilePath: user.FilePath,
+                    URLPath: user.URLPath,
+                    MobileNo: user.MobileNo,
+                };
+                result.push(userObj);
+              });
+            res.status(200).send({"data":result})
         }
         else{
             res.status(401).send({ "status": "Failed", "message": "Insufficient Permissions" });
